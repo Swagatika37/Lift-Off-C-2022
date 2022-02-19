0fa33cfa0a8b0d7ed404666cae76e7a491b48936
Swagatika37 <98446037+Swagatika37@users.noreply.github.com>
2022-02-19 17:26:10 +0530

    Create w1q3.c

---
+++ Week 1/w1q3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    
+    int num;
+    
+    printf("Enter a number \n ");
+    scanf("%d",&num);
+
+    switch(num%2){
+        case 0:
+        printf("%d is even \n",num);
+        break;
+        
+        case 1:
+        printf("%d is odd \n",num);
+        break;
+    }
+    
+    return 0;
+}
